99a92ba566630698caab37e0514279af59441dca
Hit-Bheda <hitbheda.h@gmail.com>
2024-05-30 10:57:25 +0530

    feat: The Navbar Is Created And Half Of Hero Section Is Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // import React from 'react'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
 import './Styles/style.scss'
 function App() {
   return (
     <div className="main">
-      
+      <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import './hero.scss'
+
+function Hero() {
+  return (
+    <div className='hero'>
+      <div className="hero-text">
+        <div className="masker">
+            <h1>WE CREATE</h1>
+        </div>
+        <div className="masker">
+            <div className="hero-box">
+            </div>
+            <h1>EYE-OPENING</h1>
+        </div>
+        <div className="masker">
+            <h1>PRESENTATIONS</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
