37b4ad411ade1739a495c9fba7586159f706b13d
Hit-Bheda <hitbheda.h@gmail.com>
2024-05-31 15:03:11 +0530

    feat: The Hero Section Is Completed(But It's Not Responsive) I Also Creted Marquee(It's Also Incomplete And Horrible)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // import React from 'react'
 import Hero from './Components/Hero'
+// import Marquee from './Components/Marquee'
 import Navbar from './Components/Navbar'
 import './Styles/style.scss'
 function App() {
   return (
     <div className="main">
-      <Navbar />
-      <Hero />
+      <div className="home-page">
+        <Navbar />
+        <Hero />
+      </div>
+      {/* <Marquee /> */}
     </div>
   )
 }
---
+++ src/Components/Marquee/Marquee.tsx
@@ -0,0 +1,14 @@
+import './marquee.scss';
+
+function Marquee() {
+  return (
+    <div className='marquee'>1
+      <div className="marquee-text">
+        <h1>WE ARE OCHI</h1>
+        <h1>WE ARE OCHI</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Marquee
