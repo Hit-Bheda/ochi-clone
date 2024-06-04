7668849205c9e92640c33b4c659d0c250a5fd0b2
Hit-Bheda <hitbheda.h@gmail.com>
2024-06-04 19:28:37 +0530

    feat: Just Creted Files For Projects Section And Added Boilerplate

---
+++ src/Components/Projects/Card.tsx
@@ -0,0 +1,13 @@
+import './projects.scss'
+
+function Card() {
+  return (
+    <div className='card'>
+      <div className="card-heading"></div>
+      <div className="card-body"></div>
+      <div className="card-tags"></div>
+    </div>
+  )
+}
+
+export default Card
